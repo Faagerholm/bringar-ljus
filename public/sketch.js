faad43d33c84d8e45e19f6467d3ed3c82884ce99
Jimmy Fagerholm <jimmy@identio.fi>
2021-02-26 09:29:25 +0200

    FullScreen canvas, reset style

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,7 @@
 let selected = [0,];
-const width = 5, height = 5;
+const width = 100, height = 100;
+const cellWidth = window.innerWidth / width;
+const cellHeight = window.innerHeight / height;
 let socket;
 let color;
 
@@ -14,7 +16,7 @@ const initRandomColor = () => {
 
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(window.innerWidth, window.innerHeight);
   socket = io();
   socket.on('update', items => {
     selected = items;
@@ -31,8 +33,8 @@ function draw() {
 
   for (let y = 0; y <= width; y++) {
     for (let x = 0; x <= height; x++) {
-      let xpos = x * 100;
-      let ypos = y * 100;
+      let xpos = x * cellWidth;
+      let ypos = y * cellHeight;
       let index = y * width + x; // find the index
       
       const pixel = selected.find((item) => item.index === index);
@@ -43,24 +45,22 @@ function draw() {
       }
 
       stroke(0);
-      rect(xpos, ypos, 100, 100);
-
+      rect(xpos, ypos, cellWidth, cellHeight);
       let h = map(index, 0, 69, 0, 255);
       fill(h);
       noStroke();
-      text(index, xpos, ypos, 100, 100);
+      text(index, xpos, ypos, cellWidth, cellHeight);
 
     }
   }
 }
 
 function mouseClicked() {
-  const x = parseInt(mouseX / 100);
-  const y = parseInt(mouseY / 100);
-
+  const x = parseInt(mouseX / cellWidth);
+  const y = parseInt(mouseY / cellHeight);
+  console.log(x, y);
   if (x < height && y < width) {
     const index = (y * width + x);
-    
-    socket.emit('select', {index, color});
+    socket.emit('select', {index: index, color: color});
   }
 }
